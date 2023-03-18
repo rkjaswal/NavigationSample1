f34b7f8ebc0668261cc61f781f12fb7d457d5435
Rajeev Jaswal <rkjaswal@gmail.com>
2023-03-18 12:19:34 +0000

    Initial Version

---
+++ NavigationSample1/NavigationSample1App.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSample1App.swift
+//  NavigationSample1
+//
+//  Created by Rajeev Jaswal on 18/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSample1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
